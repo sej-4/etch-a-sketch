b7354554f7c6830f962b6cbf2b74c573f0c46ce9
Sergio Joya <284065931+sej-4@users.noreply.github.com>
2026-08-06 16:03:09 -0400

    Remove nested loop

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const div = document.createElement("div");
-    container.append(div);
-  }
+const gridSize = 16 ** 2;
+
+for (let i = 0; i < gridSize; i++) {
+  const div = document.createElement("div");
+  container.append(div);
 }
