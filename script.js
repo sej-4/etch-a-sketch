698bbfadc472dc3e0aaade8cd9946ba951652602
Sergio Joya <284065931+sej-4@users.noreply.github.com>
2026-08-06 16:43:42 -0400

    Prompt user for new grid size

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container");
-
 const gridSize = 16 ** 2;
+const button = document.querySelector("button");
 
 for (let i = 0; i < gridSize; i++) {
   const div = document.createElement("div");
@@ -9,4 +9,23 @@ for (let i = 0; i < gridSize; i++) {
 
 container.addEventListener("mouseover", (event) => {
   event.target.classList.add("fill-square");
+});
+
+function isValidInput(numberOfSquares) {
+  return (
+    numberOfSquares &&
+    numberOfSquares > 0 &&
+    numberOfSquares <= 100 &&
+    Number.isInteger(numberOfSquares)
+  );
+}
+
+button.addEventListener("click", () => {
+  const numberOfSquares = +prompt(
+    "Enter the number of squares per side (Max: 100)",
+  );
+
+  if (isValidInput(numberOfSquares)) {
+    container.textContent = "";
+  }
 });
